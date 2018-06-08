df823b3bb07472b19b82232f108ae8bfccf4105e
m1zu <m1zu@users.noreply.github.com>
2018-06-08 23:21:50 +0200

    added Brick class

---
+++ Engine/Brick.cpp
@@ -0,0 +1,36 @@
+#include "Brick.h"
+#include <assert.h>
+
+Brick::Brick(const Vec2 & upperLeft, Color c)
+	:
+	rect(upperLeft, width, height, c)
+{
+	assert(!initialized);
+	initialized = true;
+}
+
+void Brick::Draw(Graphics & gfx)
+{
+	assert(initialized);
+	if (!isDestroyed)
+		rect.Draw(gfx, 1);
+}
+
+void Brick::Update(Ball & ball)
+{
+	assert(initialized);
+	if (!isDestroyed)
+	{
+		bool top, bottom, left, right;
+		const Rect& ballRect = ball.getRect();
+		isDestroyed = rect.checkCollision(ballRect, top, bottom, left, right);
+		if (top)
+			ball.ReboundY(rect.upperLeft.y - 1.0f - ball.halfWidth * 2.0f);
+		if (bottom)
+			ball.ReboundY(rect.upperLeft.y + rect.height);
+		if (left)
+			ball.ReboundX(rect.upperLeft.x - 1.0f - ball.halfWidth * 2.0f);
+		if (right)
+			ball.ReboundX(rect.upperLeft.x + rect.width);
+	}
+}
---
+++ Engine/Brick.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Rect.h"
+#include "Graphics.h"
+#include "Ball.h"
+
+class Brick {
+public:
+	Brick() = default;
+	Brick(const Vec2& upperLeft, Color c);
+
+	void Draw(Graphics& gfx);
+	void Update(Ball& ball);
+
+public:
+	static constexpr float width = 40.0f;
+	static constexpr float height = 20.0f;
+
+private:
+	bool isDestroyed = false;
+	bool initialized = false;
+	Rect rect;
+};
